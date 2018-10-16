82604696a2c0a87db9347afc9229b410f5eaaf16
Zihan Ban <zihan.ban@sv.cmu.edu>
2018-10-15 23:14:42 -0700

    don'

---
+++ LocationService/LocationService/Place.swift
@@ -0,0 +1,13 @@
+//
+//  Place.swift
+//  LocationService
+//
+//  Created by 班子寒 on 10/4/18.
+//  Copyright © 2018 ZihanBan. All rights reserved.
+//
+
+import UIKit
+
+class Place: NSObject {
+
+}
